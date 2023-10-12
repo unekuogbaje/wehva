45240ae8010090719ba3d346d466998193781983
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-10-12 21:25:43 +0100

    fetching from the API

--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -4,7 +4,24 @@ import '../components/style.css'
 
 function SearchMain() {
   const [searchTerm, setSearchTerm] = useState("Lagos");
-  console.log(searchTerm);
+  
+  const getWeatherInfo = async () => {
+    try{
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=003cc376b1d5b53cf7682e38515a0663`;
+      
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=003cc376b1d5b53cf7682e38515a0663
+
+  useEffect( () =>{
+    getWeatherInfo()
+  }, [])
   return (
     <Fragment>
       <div className="wrap">
@@ -13,9 +30,10 @@ function SearchMain() {
             type="search"
             placeholder="Search city.."
             id="search"
+            value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <button className="searchButton">
+          <button className="searchButton" onClick={getWeatherInfo}>
             Search
           </button>
         </div>
